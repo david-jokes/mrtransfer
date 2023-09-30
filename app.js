76c921a93279316b1f4efdc919c12a07acb31547
David Jokeš <54284056+david-jokes@users.noreply.github.com>
2023-09-30 21:37:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
+const body = document.querySelector("body")
+const darklight = document.querySelector("#darklight")
+
+const links = document.querySelectorAll(".links")
+
 const burger = document.querySelector(".burger")
 const nav = document.querySelector(".nav-menu")
 const cary = document.querySelectorAll(".cara")
 
 burger.addEventListener("click", function(){
+    nav.classList.toggle("show-nav");
+    burger.classList.toggle("rotate")
+    cary.forEach(function(e){
+        e.classList.toggle("dark")
+    })
+})
+
+const slovo = document.querySelector("#slovo")
+
+darklight.addEventListener("click", function(){
+    body.classList.toggle("light-mode")
+    links.forEach(function(e){
+        e.classList.toggle("pokus")
+    })
+
     nav.classList.toggle("show-nav");
     burger.classList.toggle("rotate")
     cary.forEach(function(e){
